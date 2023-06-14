6742b9785f020eb2e497653f424412a6bc351158
Rohaan28 <123399607+Rohaan28@users.noreply.github.com>
2023-06-15 01:29:20 +0200

    generated req.txt and modified visual

--- Application_with_visual/visual.py
+++ Application_with_visual/visual.py
@@ -1,34 +1,47 @@
+#working visual.py
 import plotly.graph_objs as go
+import plotly.offline as pyo
 
-def update_chart(data):
-    # Declare figure
-    fig = go.Figure()
+# Declare figure and trace outside the function
+fig = go.Figure()
+trace = go.Candlestick(x=[], open=[], high=[], low=[], close=[], name='market data')
+fig.add_trace(trace)
 
-    # Candlestick
-    fig.add_trace(go.Candlestick(x=data['Date'],
-                    open=data['Open'],
-                    high=data['High'],
-                    low=data['Low'],
-                    close=data['Close'], name='market data'))
+# Set up web server
+web_dir = "web"
+html_file = "/Users/Rohaan/Desktop/DE/Data_Engineering_Project/chart.html"
 
-    # Add titles
-    fig.update_layout(
-        title='Uber live share price evolution',
-        yaxis_title='Uber Price (kUS Dollars)')
+def update_chart(df):
+    if not df.empty:
+        # Update the trace data with the new candlestick data
+        fig.data[0].x = df.index
+        fig.data[0].open = df['Open']
+        fig.data[0].high = df['High']
+        fig.data[0].low = df['Low']
+        fig.data[0].close = df['Close']
 
-    # X-Axes
-    fig.update_xaxes(
-        rangeslider_visible=True,
-        rangeselector=dict(
-            buttons=list([
-                dict(count=15, label="15m", step="minute", stepmode="backward"),
-                dict(count=45, label="45m", step="minute", stepmode="backward"),
-                dict(count=1, label="HTD", step="hour", stepmode="todate"),
-                dict(count=6, label="6h", step="hour", stepmode="backward"),
-                dict(step="all")
-            ])
+        # Update the layout if necessary
+        fig.update_layout(
+            title='Bitcoin live share price evolution',
+            yaxis_title='Bitcoin Price (kUS Dollars)'
         )
-    )
 
-    # Show the chart
-    fig.show()
+        # Update the X-Axes if necessary
+        fig.update_xaxes(
+            rangeslider_visible=True,
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=15, label="15m", step="minute", stepmode="backward"),
+                    dict(count=45, label="45m", step="minute", stepmode="backward"),
+                    dict(count=1, label="HTD", step="hour", stepmode="todate"),
+                    dict(count=6, label="6h", step="hour", stepmode="backward"),
+                    dict(step="all")
+                ])
+            )
+        )
+
+        # Save the updated graph as an HTML file
+        pyo.plot(fig, filename = html_file, auto_open=False)
+
+    else:
+        print('No data available')
